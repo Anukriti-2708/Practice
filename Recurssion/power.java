e187059e03c1736e569667b50ecc7faab50b57d0
Anukriti-2708 <saxenaanukriti91@gmail.com>
2021-11-04 01:10:28 +0530

    power added

---
+++ Recurssion/power.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class power {
+    public static void main(String[]args){
+
+       Scanner sn = new Scanner(System.in);
+       int x = sn.nextInt();
+       int n = sn.nextInt();
+       int xn= square(x,n);
+
+      System.out.println(xn);
+   
+    }
+    public static int square(int x,int n){
+        if(n==0){
+            return 1;
+        }
+        int xn = square (x,n-1);
+        int xnx = x*xn;
+        return xnx;
+
+           
+    }
+}
